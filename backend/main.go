0eccacfe715395bcecaf8655113b4b399bb9ce45
patryk-kurek <patryk.kurek@wpengine.com>
2024-12-14 15:11:56 +0100

    feat: env handler

--- backend/main.go
+++ backend/main.go
@@ -3,19 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"backend/handlers"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	handlers.RegisterStepsRoutes(r)
+    // Load environment variables from .env file
+    if err := godotenv.Load(); err != nil {
+        log.Fatal("Error loading .env file")
+    }
 
-	port := ":3000"
-	log.Printf("Server running on port %s", port)
-	http.ListenAndServe(port, r)
+    r := chi.NewRouter()
+    r.Use(middleware.Logger)
+    handlers.RegisterStepsRoutes(r)
+
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "3000"
+    }
+    log.Printf("Server running on port %s", port)
+    http.ListenAndServe(":"+port, r)
 }
