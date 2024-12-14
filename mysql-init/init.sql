82b469e8c54d2a61f2953319c5c78b38584e2877
patryk-kurek <patryk.kurek@wpengine.com>
2024-12-14 16:35:35 +0100

    feat: init.sql updated

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -6,3 +6,17 @@ CREATE TABLE IF NOT EXISTS icons (
     description TEXT,
     tag VARCHAR(255)
 );
+
+CREATE TABLE IF NOT EXISTS tags (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Insert tags
+INSERT INTO tags (name) VALUES ('whatsapp');
+INSERT INTO tags (name) VALUES ('ios_system');
+INSERT INTO tags (name) VALUES ('android_system');
+INSERT INTO tags (name) VALUES ('messenger');
+INSERT INTO tags (name) VALUES ('instagram');
+INSERT INTO tags (name) VALUES ('facebook');
+INSERT INTO tags (name) VALUES ('google_maps');
