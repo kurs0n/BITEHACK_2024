585239f3bae2db4a72076f2e3a1f7b9da8eb922c
Maciej Klimek <maciej.klimek09@gmail.com>
2024-12-14 19:19:13 +0100

    basic styling for task list, mainly tailwing work

--- client/src/TechnicalHelp/Task.tsx
+++ client/src/TechnicalHelp/Task.tsx
@@ -1,15 +1,28 @@
 type TaskProps = {
-    index: number;
-    description: string;
-  };
+  index: number;
+  description: string;
+  icon?: string; // Add the icon as an optional prop
+};
 
-const Task = ({ index, description }: TaskProps) => {
-
-    return (
-        <li>
-        {index}. {description}
-        </li>
-    );
+const Task: React.FC<TaskProps> = ({ index, description, icon }) => {
+  return (
+    <div className="group">
+      <span className="text-sm text-stone-500 opacity-0 transition-opacity duration-200 group-hover:opacity-100">
+        Kliknij by uzyskac wiecej informacji
+      </span>
+      <li
+        className="flex flex-
+      col items-start bg-1 p-2 pl-6 border-stone-800 border-l-4 rounded-r-xl w-fit"
+      >
+        {" "}
+        <div className="flex items-center space-x-4">
+          <span className="text-2xl font-semibold text-stone-800 mr-4 ">{index}.</span>
+          <span>{description}</span>
+          {icon && <img src={icon} alt={`icon-${index}`} className="h-12 rounded-xl" />}
+        </div>
+      </li>
+    </div>
+  );
 };
 
 export default Task;
