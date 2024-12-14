bc6f504dd75a8eb08689f8741b90ea8c9459cf3e
Maciej Klimek <maciej.klimek09@gmail.com>
2024-12-14 23:39:17 +0100

    added better scrollbars

--- client/src/TechnicalHelp/Task.tsx
+++ client/src/TechnicalHelp/Task.tsx
@@ -39,8 +39,7 @@ const Task: React.FC<TaskProps> = ({ index, description, icon }) => {
         }
       } catch (error) {
         console.error("Error fetching detailed step:", error);
-      }
-      finally{
+      } finally {
         setIsLoading(false);
       }
     }
@@ -51,42 +50,43 @@ const Task: React.FC<TaskProps> = ({ index, description, icon }) => {
 
   return (
     <div className="group h-full relative">
-    {/* Wrap the li in a div with full height */}
-    <span className="text-sm text-stone-500 opacity-0 transition-opacity duration-200 group-hover:opacity-100">
-      Kliknij by uzyskac wiecej informacji
-    </span>
-  
-    <div className={`h-full bg-2 rounded-r-2xl relative`}>
-      {/* Blur Overlay */}
-      {isLoading && (
-        <div className="absolute inset-0 backdrop-blur-sm rounded-r-2xl z-10"></div>
-      )}
-  
-      <li
-        className={`flex flex-col items-start bg-1 p-2 px-6 border-stone-800 border-l-4 rounded-r-xl w-full transition-all duration-500 ease-in-out ${
-          isExpanded ? "w-full" : "w-fit"
-        }`}
-        onClick={handleTaskClick}
-      >
-        <div className="flex items-center space-x-4">
-          <span className="text-2xl font-semibold text-stone-800 mr-4">{index}.</span>
-          <span>{description}</span>
-          {icon && (
-            <img
-              src={icon}
-              alt={`icon-${index}`}
-              className="h-12 rounded-xl ml-auto" // Move icon to the right
-            />
-          )}
-        </div>
-        {isExpanded && detailedStep && (
-          <div className="text-stone-600 mx-12 my-3 py-4 border-t-2 border-stone-800">
-            {detailedStep} {/* Display the detailed step under the task */}
+      {/* Wrap the li in a div with full height */}
+      <span className="text-sm text-stone-500 opacity-0 transition-opacity duration-200 group-hover:opacity-100">
+        Kliknij by uzyskac wiecej informacji
+      </span>
+
+      <div className={`h-full bg-2 rounded-r-2xl relative`}>
+        {/* Blur Overlay */}
+        {isLoading && (
+          <div className="absolute inset-0 backdrop-blur-sm rounded-r-2xl z-10 flex items-center justify-center text-xl">
+            Ładowanie...
           </div>
         )}
-      </li>
+        <li
+          className={`flex flex-col items-start bg-1 p-2 px-6 border-stone-800 border-l-4 rounded-r-xl transition-all duration-500 ease-in-out ${
+            isExpanded ? "w-full" : "w-fit"
+          }`}
+          onClick={handleTaskClick}
+        >
+          <div className="flex items-center space-x-4">
+            <span className="text-2xl font-semibold text-stone-800 mr-4">{index}.</span>
+            <span>{description}</span>
+            {icon && (
+              <img
+                src={icon}
+                alt={`icon-${index}`}
+                className="h-12 rounded-xl ml-auto" // Move icon to the right
+              />
+            )}
+          </div>
+          {isExpanded && detailedStep && (
+            <div className="text-stone-600 mx-12 my-3 py-4 border-t-2 border-stone-800">
+              {detailedStep} {/* Display the detailed step under the task */}
+            </div>
+          )}
+        </li>
+      </div>
     </div>
-  </div>  
   );
 };
 
