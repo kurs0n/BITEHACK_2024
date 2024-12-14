a22b7486c20a4c93c71c09d07c9ca8b50c890340
Maciej <maciekjur123@gmail.com>
2024-12-14 15:53:45 +0100

    xd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,7 @@
+import Container from "./TechnicalHelp/Container";
 function App() {
   return (
-    <div>
-      <div>
-        <h1>Witaj, w czym mogę ci dzisiaj pomóc?</h1>
-
-        <div>
-          <span>Jak zmienić czcionkę na większą?</span>
-          <span>➤</span>
-        </div>
-
-        <ol>
-          <li>Kliknij w ikonę ustawień</li>
-          <li>Wpisz w pasku wyszukiwania "czcionka"</li>
-          <li>Kliknij w pierwszą opcję od góry</li>
-          <li>Kliknij w opcje "Czcionka i Rozmiar"</li>
-          <li>Dostosuj suwakiem preferowany rozmiar</li>
-        </ol>
-      </div>
-    </div>
+    <Container></Container>
   );
 }
 
