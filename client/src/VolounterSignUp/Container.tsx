a76843ed243cb08f763aa31de57749c12264d52f
Maciej Klimek <maciej.klimek09@gmail.com>
2024-12-15 09:24:54 +0100

    styling changes, added basic responsivness

--- client/src/VolounterSignUp/Container.tsx
+++ client/src/VolounterSignUp/Container.tsx
@@ -1,25 +1,24 @@
 import React from "react";
-import VolunteerSignUoForms from "./VolounterSignUpForms";
+import VolunteerSignUpForm from "./VolounterSignUpForms";
 import { Link } from "react-router-dom";
 
 const VolunteerSignUpContainer: React.FC = () => {
   return (
-    <div className="bg-trans flex flex-col justify-center items-center bg-stone-300">
+    <div className="bg-trans flex flex-col justify-center items-center w-full h-full px-4 sm:px-6 md:px-8">
       <Link
         to="/"
-        className="absolute top-6 left-6 text-stone-800 text-3xl p-4 bg-1 rounded-full shadow-lg border-2 border-stone-800 hover:bg-amber-50 transition"
+        className="absolute top-6 left-6 text-stone-800 text-3xl p-4 bg-1 rounded-full shadow-lg border-2 border-stone-800 hover:bg-amber-50 transition hidden lg:block"
       >
         <i className="fa-solid fa-arrow-left"></i>
       </Link>
-      <h1 className="text-4xl font-bold mb-6 text-stone-800">Pomoc Wolontariusza</h1>
-      <p className="text-lg text-stone-600 text-center max-w-2xl font-bold">
+      <p className="text-lg text-stone-600 text-center max-w-2xl font-bold mb-4">
         Dziękujemy za gotowość do pomocy seniorom!
       </p>
-      <p className="text-lg text-stone-600 text-center max-w-2xl">
-        Prosimy o wypełnienie poniższego formularza. Gdy Twoje wsparcie będzie potrzebne, senior skontaktuje się z Tobą.
-      </p>
       <div className="mt-8"></div>
-      <VolunteerSignUoForms />
+      <VolunteerSignUpForm />
+      <p className="text-lg text-stone-500 text-center max-w-2xl mt-12">
+        Prosimy o wypełnienie powyższego formularza. Gdy Twoje wsparcie będzie potrzebne, senior skontaktuje się z Tobą.
+      </p>
     </div>
   );
 };
--- client/src/components/Volunteer.tsx
+++ client/src/components/Volunteer.tsx
@@ -5,16 +5,16 @@ import background from "../../public/background.png";
 const Volunteer: React.FC = () => {
   return (
     <div
-    className="h-screen w-full flex justify-center items-center bg-stone-300"
-    style={{
-      backgroundImage: `url(${background})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      backgroundRepeat: "no-repeat",
-    }}>
-      <Container/>
+      className="h-screen w-full flex justify-center items-center bg-stone-300 px-0"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Container />
     </div>
-  
   );
 };
 
